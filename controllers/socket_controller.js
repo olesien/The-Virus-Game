e51f92244404662a13eca67c840881e0dc31e1e9
olesien <olesien2@gmail.com>
2022-03-15 18:37:50 +0100

    Forgot to delete users variable

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -4,9 +4,7 @@
 
 const debug = require("debug")("game:socket_controller");
 
-//list of sockets ids and hteir usernames
-const users = {};
-
 module.exports = function (socket) {
+	//Log when user connects
 	debug("a new client has connected", socket.id);
 };
