89fc7a75ef3c987fe73ddda92a4efa33c0777538
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2022-03-19 20:34:51 +0100

    New clock

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -6,7 +6,8 @@ const gridBoxes = document.querySelectorAll(".grid-box");
 const gameBoardTitle = document.querySelector(".main-window__bar-link");
 const roundsEl = document.querySelector("#rounds");
 const scoreboardEl = document.querySelector('.scoreboard-list');
-
+const loadingIcon = document.querySelector('.loading');
+const inputBtn = document.querySelector(".start-page__input-btn");
 let activeRoom = null;
 let username = null;
 
@@ -39,22 +40,48 @@ const addVirus = (randomNumber) => {
 		}
 	});
 };
-const newRoundTimer = () => {
-	gameBoardTitle.style.textAlign = 'center';
-	gameBoardTitle.textContent = 'Time Until Game Starts 5 Seconds';
-	let timer = 4, // seconds
-		seconds,
-		inter = setInterval(() => {
+
+// Lobby Clock Function
+const clock = {
+	totalSeconds: 0,
+	startTimerForLobby: function () {
+		if (!this.interval) {
+			const self = this;
+
+			function pad(val) {
+				return val > 9 ? val : "0" + val;
+			}
+
+			this.interval = setInterval(function () {
+				self.totalSeconds += 1;
+				const timerMinuts = pad(Math.floor(self.totalSeconds / 60 % 60)),
+					timerSeconds = pad(parseInt(self.totalSeconds % 60));
+				document.querySelector(".start-page__lobby-timer").textContent = `${timerMinuts}:${timerSeconds}`
+			}, 1000);
+		}
+	},
+	resetTimerForLobby: function () {
+		clock.totalSeconds = null;
+		clearInterval(this.interval);
+		document.querySelector(".start-page__lobby-timer").textContent = `00:00`
+		delete this.interval;
+	},
+	roundTimer: function () {
+		gameBoardTitle.style.textAlign = 'center';
+		gameBoardTitle.textContent = 'Time Until Game Starts 5 Seconds';
+		let timer = 4, // seconds
+			seconds;
+		let inter = setInterval(() => {
 			seconds = parseInt(timer % 60, 10);
 			gameBoardTitle.textContent = `Time Until Game Starts ${seconds} Seconds`;
-			if (timer-- < 1) {
-				gameBoardTitle.textContent =
-					"Virus can appear at any moment, be ready!";
-				clearInterval(inter);
-			}
+			if (timer-- < 1) gameBoardTitle.textContent = "Virus can appear at any moment, be ready!";clearInterval(inter);
 		}, 1000);
-};
+	},
+	resetRoundTimer: function () {
 
+	}
+
+};
 
 const startGame = (match, friend, foe) => {
 	//match contains everything needed to set up scoreboard etc, use "opponent" as key for foe
@@ -87,7 +114,7 @@ const startGame = (match, friend, foe) => {
 	scoreboardEl.appendChild(liEl);
 
 	// Round Timer
-	newRoundTimer();
+	clock.roundTimer()
 };
 
 //New round received, start new round with new virus!
@@ -119,35 +146,64 @@ socket.on("game:roundresult", (game) => {
 
 	// check which person won and lose
 	if (game.rounds[game.rounds.length - 1].winner === player) {
-		gameBoardTitle.textContent = `Win: ${
-			Math.floor(player_opponent - player_you) / 1000
-		} Seconds`;
-		setTimeout(newRoundTimer, 800);
+		gameBoardTitle.textContent = `Win: ${Math.floor(player_opponent - player_you) / 1000} Seconds`;
+		setTimeout(clock.roundTimer, 800);
+		clock.roundTimer()
 	} else if (game.rounds[game.rounds.length - 1].loser === player) {
-		gameBoardTitle.textContent = `Lose: ${
-			Math.floor(player_you - player_opponent) / 1000
-		} Seconds`;
-		setTimeout(newRoundTimer, 800);
+		gameBoardTitle.textContent = `Lose: ${Math.floor(player_you - player_opponent) / 1000} Seconds`;
+		setTimeout(clock.roundTimer, 800);
 	}
 });
 
 //All 10 rounds done, end game
 socket.on("game:end", (game) => {
-	alert("Game ended!");
 	console.log(game);
 
-	const rematchEl = document.createElement('button');
-	const lobbyEl = document.createElement('button');
+	/*
+		const rematchEl = document.createElement('button');
+		const lobbyEl = document.createElement('button');
 
-	rematchEl.classList.add('rematch');
-	lobbyEl.classList.add('lobby');
+		rematchEl.classList.add('rematch');
+		lobbyEl.classList.add('lobby');
 
-	rematchEl.innerHTML = "Rematch"
-	lobbyEl.innerHTML = "Go To Lobby"
+		rematchEl.innerHTML = "Rematch"
+		lobbyEl.innerHTML = "Go To Lobby"
 
-	scoreboardWrapperEl.append(rematchEl);
-	scoreboardWrapperEl.append(lobbyEl);
+		scoreboardWrapperEl.append(rematchEl);
+		scoreboardWrapperEl.append(lobbyEl);
+	*/
 
+
+	const gameOver = document.querySelector('.game-over'),
+		gameOverBtnReturnToLobby = document.querySelector('.game-over__btn-return-to-lobby'),
+		gameOverBtnGoAgain = document.querySelector('.game-over__btn-go-again')
+
+
+	//	show Game over screen
+	appEl.classList.toggle('hide');
+	gameOver.classList.toggle('hide');
+
+
+	gameBoardTitle.textContent = 'https://thevirusgame.com'
+// Return to Lobby
+	gameOverBtnReturnToLobby.addEventListener('click', () => {
+		gameOver.classList.toggle('hide');
+		startPageEl.classList.toggle('hide');
+		gameBoardTitle.textContent = 'https://thevirusgame.com';
+		gameBoardTitle.style.textAlign = 'left';
+		document.querySelector(".start-page__input-btn").textContent = "Start Searching";
+		document.querySelector(".start-page__title").textContent = "Enter your Name to play";
+		loadingIcon.classList.add('hide');
+		inputBtn.appendChild(loadingIcon);
+		clock.resetTimerForLobby();
+	});
+// Go Again
+	gameOverBtnGoAgain.addEventListener('click', () => {
+		gameOver.classList.toggle('hide');
+		appEl.classList.toggle('hide');
+
+
+	})
 });
 
 //Game now has the match info including opponent etc, and will start setting up all required details
@@ -169,6 +225,7 @@ socket.on("user:foundmatch", (partner) => {
 	console.log("active room: " + activeRoom);
 });
 
+
 // get username and room from form and emit `user:joined` and then show chat
 messageForm.addEventListener("submit", (e) => {
 	e.preventDefault();
@@ -187,52 +244,24 @@ messageForm.addEventListener("submit", (e) => {
 			activeRoom = status.partner ? status.partner?.room : null;
 			console.log("ACTIVE ROOM: --- " + activeRoom);
 
+			// Start lobby Timer
+			clock.startTimerForLobby();
+
+			// Change  Text for Title and Button
+			document.querySelector(".start-page__title").textContent = "Lobby status 1/2";
+			inputBtn.textContent = "Please wait for second player";
+			// Loading spinner Proportions and apply to button
+			loadingIcon.classList.remove('hide')
+			inputBtn.appendChild(loadingIcon)
+
 			if (activeRoom) {
 				//Found match already, someone was waiting
 				//startGame(status.partner.username);  //Partially Replaced by game:start
 
 				console.log("active room: " + activeRoom);
-			} else {
-				const createElImg = document.createElement("img"),
-					startSearching = document.querySelector(".start-page__input-btn"),
-					startPageLobbyTimer = document.querySelector(".start-page__lobby-timer");
-
-
-				// timer props
-				let minuts,
-					seconds,
-					hours,
-					total = 0;
-
-				// timer logic + adding to page
-				const setTime = () => {
-					total++;
-					seconds = getZero(total % 60);
-					minuts = getZero(parseInt(total / 60));
-					hours = getZero(parseInt(total / 60 / 60));
-					startPageLobbyTimer.textContent = `${hours}:${minuts}:${seconds}`;
-				};
-				// call function every seconds
-				setInterval(setTime, 1000);
-
-				// get zero if number 9 or less
-				const getZero = (num) => {
-					if (num >= 0 && num < 10) return "0" + num;
-					else return num;
-				};
-
-				// Change  Text for Title and Button
-				document.querySelector(".start-page__title").textContent = "Lobby status 1/2";
-				document.querySelector(".start-page__input-btn").textContent =
-					"Please wait for second player";
-
-				// Loading spinner Proportions and apply to button
-				createElImg.src = "../assets/icons/spinner.gif";
-				createElImg.classList.add("d-block");
-				createElImg.style.marginLeft = "10px"
-				createElImg.width = 24;
-				startSearching.appendChild(createElImg);
 			}
 		}
 	});
 });
+
+
